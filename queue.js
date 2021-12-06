321d892c2536f5f68b1385f9979146e93ace8bde
1365825528@qq.com <ChenXue3@sfmail.sf-express.com>
2021-12-06 18:15:59 +0800

    栈,队列

---
+++ queue.js
@@ -0,0 +1,52 @@
+class Queue {
+  constructor() {
+    this.count = 0;
+    this.lowestCount = 0;
+    this.items = {};
+  }
+  enqueue(element) {
+    this.items[this.count] = element;
+    this.count++;
+  }
+  dequeue() {
+    if (this.isEmpty) {
+      return undefined;
+    }
+    const result = this.items[this.lowestCount]
+    delete this.items[this.lowestCount];
+    this.lowestCount++;
+    return result;
+  }
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[this.lowestCount];
+  }
+  isEmpty() {
+    return this.size() === 0;
+  }
+  size() {
+    return this.count - this.lowestCount;
+  }
+  clear() {
+    this.items = {};
+    this.count = 0;
+    this.lowestCount = 0;
+  }
+  toString() {
+    if (this.isEmpty()) {
+      return '';
+    }
+    let objString = '';
+    for(let i = this.lowestCount; i < this.count; i++) {
+      objString += `${this.items[i]}`
+    }
+    return objString;
+  }
+}
+
+const queue = new Queue();
+queue.enqueue('John');
+queue.enqueue('Jack');
+console.log(queue.toString());
---
+++ stack.js
@@ -0,0 +1,53 @@
+class Stack {
+  constructor() {
+    this.items = {};
+    this.count = 0;
+  }
+  push(element) {
+    this.items[this.count] = element;
+    this.count++;
+  }
+  pop(element) {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    console.log(this.count);
+    this.count--;
+    console.log(this.count);
+    const result = this.items[this.count];
+    delete this.items[this.count];
+    return result;
+  }
+  peek() {
+    if(this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[this.count - 1];
+  }
+  isEmpty() {
+    return this.count === 0;
+  }
+  size() {
+    return this.count;
+  }
+  clear() {
+    this.items = {};
+    this.count = 0;
+  }
+  toString() {
+    if(this.isEmpty()) {
+      return '';
+    }
+    let objString = '';
+    for (let i = 0; i < this.count; i++) {
+      objString += `${this.items[i]}`;
+    }
+    return objString;
+  }
+}
+
+const stack = new Stack();
+console.log(stack.isEmpty());
+stack.push(5);
+stack.push(8);
+console.log(stack.toString());
